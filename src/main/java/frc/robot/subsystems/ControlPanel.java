f47ccd719d31b4be794ad7b347976deafb86bf4e
Brandon <brandonseegraves@gmail.com>
2020-02-13 20:27:07 -0500

    Added Calibration Buttons to CP Network Table
    The values are setting properly. We need to get the values and make them persistent as well as display them under the buttons on the network table.

--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -13,8 +13,12 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -29,18 +33,55 @@ public class ControlPanel extends SubsystemBase {
   private final ColorSensorV3 controlPanelColorSensor = new ColorSensorV3(controlPanelColorSensorI2CPort);
 
   private final ColorMatch colorMatcher = new ColorMatch();
-
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
-  int proximity;
-
   
+  private double kBlueTargetRedValue = 0.0;
+  private double kBlueTargetGreenValue = 0.0;
+  private double kBlueTargetBlueValue = 0.0;
+
+  private double kGreenTargetRedValue = 0.0;
+  private double kGreenTargetGreenValue = 0.0;
+  private double kGreenTargetBlueValue = 0.0;
+
+  private double kRedTargetRedValue = 0.0;
+  private double kRedTargetGreenValue = 0.0;
+  private double kRedTargetBlueValue = 0.0;
+
+  private double kYellowTargetRedValue = 0.0;
+  private double kYellowTargetGreenValue = 0.0;
+  private double kYellowTargetBlueValue = 0.0;
+
+  private final Color kBlueTarget = ColorMatch.makeColor(kBlueTargetRedValue, kBlueTargetGreenValue, kBlueTargetBlueValue);
+  private final Color kGreenTarget = ColorMatch.makeColor(kGreenTargetRedValue, kGreenTargetGreenValue, kGreenTargetBlueValue);
+  private final Color kRedTarget = ColorMatch.makeColor(kRedTargetRedValue, kRedTargetGreenValue, kRedTargetBlueValue);
+  private final Color kYellowTarget = ColorMatch.makeColor(kYellowTargetRedValue, kYellowTargetGreenValue, kYellowTargetBlueValue);
+  
+
+  //int proximity;
+  
+  boolean getRedColorCalibration = false;
+  boolean getGreenColorCalibration = false;
+  boolean getBlueColorCalibration = false;
+  boolean getYellowColorCalibration = false;
+
+
+  private ShuffleboardTab controlPanelTab = Shuffleboard.getTab("Control Panel");
+  private NetworkTableEntry ntControlPanelRedColorCalibration =
+    controlPanelTab.add("ControlPanelRedColorCalibration", getRedColorCalibration)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(2, 1).withPosition(0, 0).getEntry();     
+  private NetworkTableEntry ntControlPanelGreenColorCalibration =
+    controlPanelTab.add("ControlPanelGreenColorCalibration", getGreenColorCalibration)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(2, 1).withPosition(2, 0).getEntry();    
+  private NetworkTableEntry ntControlPanelBlueColorCalibration =
+    controlPanelTab.add("ControlPanelBlueColorCalibration", getBlueColorCalibration)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(2, 1).withPosition(4, 0).getEntry();
+  private NetworkTableEntry ntControlPanelYellowColorCalibration =
+    controlPanelTab.add("ControlPanelYellowColorCalibration", getYellowColorCalibration)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(2, 1).withPosition(6, 0).getEntry();
+    
   public ControlPanel(Joystick operatorJoystick) {
     this.operatorJoystick = operatorJoystick;
     
-    proximity = controlPanelColorSensor .getProximity();
+    //proximity = controlPanelColorSensor .getProximity();
     colorMatcher.addColorMatch(kBlueTarget);
     colorMatcher.addColorMatch(kGreenTarget);
     colorMatcher.addColorMatch(kRedTarget);
@@ -51,7 +92,42 @@ public class ControlPanel extends SubsystemBase {
   @Override
   public void periodic() {
     Color detectedColor = controlPanelColorSensor.getColor();
+    System.out.println("Blue Color Value " + kBlueTarget);
+    System.out.println("Boolean State " + getBlueColorCalibration);
+    System.out.println("Blue R Value " + kBlueTargetRedValue + 
+      " Blue Green Value " + kBlueTargetGreenValue + " Blue Blue Value " + kBlueTargetBlueValue);
+    if(ntControlPanelRedColorCalibration.getBoolean(false))
+    {
+      kRedTargetRedValue = detectedColor.red;
+      kRedTargetGreenValue = detectedColor.green;
+      kRedTargetBlueValue = detectedColor.blue;
+      ntControlPanelRedColorCalibration.setBoolean(false);
+    }
     
+    if(ntControlPanelGreenColorCalibration.getBoolean(false))
+    {
+      kGreenTargetRedValue = detectedColor.red;
+      kGreenTargetGreenValue = detectedColor.green;
+      kGreenTargetBlueValue = detectedColor.blue; 
+      ntControlPanelGreenColorCalibration.setBoolean(false);
+    }
+
+    if(ntControlPanelBlueColorCalibration.getBoolean(false))
+    {
+      kBlueTargetRedValue = detectedColor.red;
+      kBlueTargetGreenValue = detectedColor.green;
+      kBlueTargetBlueValue = detectedColor.blue;
+      ntControlPanelBlueColorCalibration.setBoolean(false);
+    }
+
+    if(ntControlPanelYellowColorCalibration.getBoolean(false))
+    {
+      kYellowTargetRedValue = detectedColor.red;
+      kYellowTargetGreenValue = detectedColor.green;
+      kYellowTargetBlueValue = detectedColor.blue;
+      ntControlPanelYellowColorCalibration.setBoolean(false);
+    }
+
     //System.out.println("Red" + detectedColor.red + "Green" + detectedColor.green + "Blue" + detectedColor.blue + " Raw BLue is " + controlPanelColorSensor.getBlue());
     //System.out.println("Most likely color : " + colorSensor.getColor().);
     /*ColorMatchResult match = colorMatcher.matchClosestColor(detectedColor);
