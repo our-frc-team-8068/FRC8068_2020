2095c0ff01e7959f4efc70ed5a56157490a97420
Brandon <brandonseegraves@gmail.com>
2020-02-21 18:20:14 -0500

    Creating Shooter Branch

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,7 +10,11 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
@@ -22,6 +26,39 @@ public class Shooter extends SubsystemBase {
   private final WPI_VictorSPX preigniterVictorSPX = new WPI_VictorSPX(20);
   private final WPI_TalonSRX topShooterTalonSRX = new WPI_TalonSRX(21);
   private final WPI_TalonSRX bottomShooterTalonSRX = new WPI_TalonSRX(22);
+
+  private ShuffleboardTab shooterControlTab = Shuffleboard.getTab("ShooterControl"); 
+  
+  private NetworkTableEntry ntScdProportionalGain = 
+    shooterControlTab.add("ScdShooterProportionalGain", 69.0).withSize(2, 1).withPosition(0, 0).getEntry();
+    
+  private NetworkTableEntry ntScdUpdateProportionalGain = 
+    shooterControlTab.add("ScdShooterUpdateProportionalGain", false)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(1, 1).withPosition(2, 0).getEntry();
+  
+  private NetworkTableEntry ntStsProportionalGain = 
+    shooterControlTab.addPersistent("StsShooterProportionalGain ", 69.0).withSize(2, 1).withPosition(4, 0).getEntry();
+
+    private NetworkTableEntry ntScdIntegralGain = 
+    shooterControlTab.add("ScdShooterIntegralGain", 69.0).withSize(2, 1).withPosition(0, 1).getEntry();
+    
+  private NetworkTableEntry ntScdUpdateIntegralGain = 
+    shooterControlTab.add("ScdShooterUpdateIntegralGain", false)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(1, 1).withPosition(2, 1).getEntry();
+  
+  private NetworkTableEntry ntStsIntegralGain = 
+    shooterControlTab.addPersistent("StsShooterIntegralGain ", 69.0).withSize(2, 1).withPosition(4, 1).getEntry();  
+
+  private NetworkTableEntry ntScdDerivativeGain = 
+    shooterControlTab.add("ScdShooterDerivativeGain", 69.0).withSize(2, 1).withPosition(0, 2).getEntry();
+    
+  private NetworkTableEntry ntScdUpdateDerivativeGain = 
+    shooterControlTab.add("ScdShooterUpdateDerivativeGain", false)
+      .withWidget(BuiltInWidgets.kToggleButton).withSize(1, 1).withPosition(2, 2).getEntry();
+  
+  private NetworkTableEntry ntStsDerivativeGain = 
+    shooterControlTab.addPersistent("StsShooterDerivativeGain ", 69.0).withSize(2, 1).withPosition(4, 2).getEntry();
+
   
   public Shooter(Joystick driverJoystick) {
     this.driverJoystick = driverJoystick;
